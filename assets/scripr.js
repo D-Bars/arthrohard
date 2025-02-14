bf620cc515da5a9ff1599f13db2af9cf1cdab2c1
D-Bars <awotik26@gmail.com>
2025-02-15 00:25:36 +0100

    rework: parallax

--- assets/scripr.js
+++ assets/scripr.js
@@ -93,52 +93,12 @@ if (!isMobile) {
       menuItems.forEach(item => item.classList.remove('active__menu__link'));
 
       const activeMenuItem = menuContainer.querySelector(`a[href="#${newActiveSectionId}"]`)?.closest('.hover__underline__trigger');
-      activeMenuItem?.classList.add('active__menu__link');
+      activeMenuItem.classList.add('active__menu__link');
     }
   }
   window.addEventListener('scroll', onScroll);
   onScroll();
 }
-
-
-
-//parallax
-
-// window.addEventListener('DOMContentLoaded', () => {
-//     const dog = document.getElementById("description__product__parallax__dog");
-//     const wrapper = document.querySelector(".parallax__dog__wrapper");
-
-//     if (window.innerWidth < 800) {
-//         dog.style.transform = 'translateX(0)';
-//         return;
-//     }
-
-//     const maxMove = 7.5;  
-//     const factor = 0.005; 
-
-//     function handleParallax() {
-//         const scrollY = window.scrollY;
-//         const moveX = Math.sin(scrollY * factor) * maxMove;
-//         dog.style.transform = `translateX(${moveX}vw)`;
-//     }
-
-//     const observerParallaxOptions = {
-//         root: null,
-//         threshold: 0.2  
-//     };
-
-//     const observer = new IntersectionObserver((entries) => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 window.addEventListener("scroll", handleParallax);
-//             } else {
-//                 window.removeEventListener("scroll", handleParallax);
-//             }
-//         });
-//     }, observerParallaxOptions);
-
-//     observer.observe(wrapper);
-// });
 
 // API get products
 
